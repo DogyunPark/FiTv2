d1fa813d5fb9878689df3773dfcd668fae06cf35
koparkrea <koparkrea@gmail.com>
2025-03-04 20:43:42 +0900

    add

--- fit/model/fit_model_lwd.py
+++ fit/model/fit_model_lwd.py
@@ -361,8 +361,8 @@ class FiTLwD(nn.Module):
                 else:
                     x = self.x_embedder(x)                          # (B, N, C) -> (B, N, D)  
 
-                if self.number_of_representation_blocks > 1:
-                    x += representation_noise
+                # if self.number_of_representation_blocks > 1:
+                #     x += representation_noise
                 
                 if self.use_checkpoint:
                     if self.number_of_shared_blocks > 0:
@@ -663,8 +663,8 @@ class FiTLwD(nn.Module):
                 else:
                     x = self.x_embedder(x)                          # (B, N, C) -> (B, N, D)  
                 
-                if self.number_of_representation_blocks > 1:
-                    x += representation_noise
+                # if self.number_of_representation_blocks > 1:
+                #     x += representation_noise
 
                 if self.use_checkpoint:
                     if self.number_of_shared_blocks > 0:
